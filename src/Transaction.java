1bffce22b8b723484627e69819c6f2521d6f7b6b
Maria Kochulanova <holamarymar@gmail.com>
2020-07-30 23:24:19 +0300

    Report, programm file and screenshots were added

---
+++ src/Transaction.java
@@ -0,0 +1,10 @@
+public class Transaction {
+    public static void main(String[] args) {
+        int current = 2_000_000_000;
+        int transaction = 500_000_000;
+        int updated_sum = current + transaction;
+
+        System.out.println(updated_sum);
+
+    }
+}
